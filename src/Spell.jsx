26dacdb74329bfbaad55c15a9098cc2cea0fa55b
Aosegir <128326345+Aosegir@users.noreply.github.com>
2025-01-05 21:08:10 -0800

    Solved issue with Spell.css

--- src/Spell.jsx
+++ src/Spell.jsx
@@ -6,7 +6,7 @@ function Spells() {
         <div className="grid">
             {Data.map((spell) => {
                 return(
-                    <div key={spell.id} className="spell-item">
+                    <div key={spell.id} className="spell-item border">
                         <p>{spell.level}</p>
                         <h3>{spell.name}</h3>
                     </div>
