e5e209d6ae8b2ae4d4487291b3a431d12e81a78b
Aosegir <128326345+Aosegir@users.noreply.github.com>
2025-01-08 12:43:07 -0800

    Removed most styles from index.css. Edited root to fit screen better. Created files for header.

--- src/Spell.jsx
+++ src/Spell.jsx
@@ -3,15 +3,17 @@ import './Spell.css';
 
 function Spells() {
     return(
-        <div className="grid">
-            {Data.map((spell) => {
-                return(
-                    <div key={spell.id} className="spell-item border">
-                        <p>{spell.level}</p>
-                        <h3>{spell.name}</h3>
-                    </div>
-                );
-            })}
+        <div className="flex center">
+            <div className="grid">
+                {Data.map((spell) => {
+                    return(
+                        <div key={spell.id} className="spell-item border">
+                            <p>{spell.level}</p>
+                            <h3>{spell.name}</h3>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
