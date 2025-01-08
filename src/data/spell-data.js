f489a851f32183c474f05adf153390b285bb83d4
Aosegir <128326345+Aosegir@users.noreply.github.com>
2025-01-07 19:52:36 -0800

    Better understand mapping and passing data

--- src/data/spell-data.js
+++ src/data/spell-data.js
@@ -7,6 +7,14 @@ const Data = [
         duration: "instantaneous",
         damage: "fire",
     },
+    {
+        id: crypto.randomUUID,
+        level: 3,
+        name: "lightning bolt",
+        castingTime: "1 action",
+        duration: "instantaneous",
+        damage: "lightning",
+    },
 ];
 
 export default Data;
