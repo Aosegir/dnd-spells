0dcd9d1f268b0c3a3fa23db36474a862979cbf18
Aosegir <128326345+Aosegir@users.noreply.github.com>
2025-01-09 10:30:21 -0800

    Created header and attached it to App.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,1 +1,18 @@
 import "./Header.css";
+
+function Header() {
+    return (
+        <div className="container">
+            <h1>DND Spells Test</h1>
+            <div className="filter">
+                <p>Spell Name</p>
+                <p>Class</p>
+                <p>Spell Level</p>
+                <p>Spell School</p>
+                <p>Casting Time</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
