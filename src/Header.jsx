e41979612f20b49c9df309932aeb609dc7a6756b
Aosegir <128326345+Aosegir@users.noreply.github.com>
2025-01-09 11:53:52 -0800

    Improved spell search form

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,13 +4,33 @@ function Header() {
     return (
         <div className="container">
             <h1>DND Spells Test</h1>
-            <div className="filter">
-                <p>Spell Name</p>
-                <p>Class</p>
-                <p>Spell Level</p>
-                <p>Spell School</p>
-                <p>Casting Time</p>
-            </div>
+            <form className="spell-form">
+                <div className="column">
+                    <label htmlFor="spell-name">Spell Name</label>
+                    <input type="text" id="spell-name" />
+                </div>
+                
+                <div className="column">
+                    <label htmlFor="class">Class</label>
+                    <input type="text" id="class" />
+                </div>
+                
+                <div className="column">
+                    <label htmlFor="spell-level">Spell Level</label>
+                    <input type="text" id="spell-level" />
+                </div>
+                
+                <div className="column">
+                    <label htmlFor="spell-school">Spell School</label>
+                    <input type="text" id="spell-school" />
+                </div>
+                
+                <div className="column">
+                    <label htmlFor="casting-time">Casting Time</label>
+                    <input type="text" name="" id="casting-time" />
+                </div>
+                
+            </form>
         </div>
     );
 };
