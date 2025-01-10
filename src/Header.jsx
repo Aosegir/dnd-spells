ea14a897b0e3d644984450ee17fcfd533d9bc0c2
Aosegir <128326345+Aosegir@users.noreply.github.com>
2025-01-10 10:05:02 -0800

    Improved header title placement and established area for clear UI for spells list

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,30 +2,32 @@ import "./Header.css";
 
 function Header() {
     return (
-        <div className="container">
-            <h1>DND Spells Test</h1>
+        <div className="container flex column center-align">
+            <div className="header-flex left-justify max-width">
+                <h1>DND Spells Test</h1>
+            </div>
             <form className="spell-form">
-                <div className="column">
+                <div className="header-flex column">
                     <label htmlFor="spell-name">Spell Name</label>
                     <input type="text" id="spell-name" />
                 </div>
                 
-                <div className="column">
+                <div className="header-flex column">
                     <label htmlFor="class">Class</label>
                     <input type="text" id="class" />
                 </div>
                 
-                <div className="column">
+                <div className="header-flex column">
                     <label htmlFor="spell-level">Spell Level</label>
                     <input type="text" id="spell-level" />
                 </div>
                 
-                <div className="column">
+                <div className="header-flex column">
                     <label htmlFor="spell-school">Spell School</label>
                     <input type="text" id="spell-school" />
                 </div>
                 
-                <div className="column">
+                <div className="header-flex column">
                     <label htmlFor="casting-time">Casting Time</label>
                     <input type="text" name="" id="casting-time" />
                 </div>
--- src/Spell.jsx
+++ src/Spell.jsx
@@ -3,18 +3,25 @@ import './Spell.css';
 
 function Spells() {
     return(
-        <div className="flex center">
-            <div className="grid">
-                {Data.map((spell) => {
-                    return(
-                        <div key={spell.id} className="spell-item border">
-                            <p>{spell.level}</p>
-                            <h3>{spell.name}</h3>
-                        </div>
-                    );
-                })}
+        <div>
+            <div className="flex center">
+                <h3>Test</h3>
+            </div>
+            
+            <div className="flex center">
+                <div className="grid">
+                    {Data.map((spell) => {
+                        return(
+                            <div key={spell.id} className="spell-item border">
+                                <p>{spell.level}</p>
+                                <h3>{spell.name}</h3>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
+        
     );
 };
 
